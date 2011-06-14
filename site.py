ada08c4654a51dd062666e04ec6093d3b14468fd
Robin Norwood <robin.norwood@gmail.com>
2011-06-14 08:31:10 -0500

    Blargh. Move stuff around again.

---
+++ site.py
@@ -0,0 +1,28 @@
+import web
+
+urls = (
+  '/', 'index',
+  '/levels/(\w+)', 'levels'
+)
+
+app = web.application(urls, globals())
+
+application = app.wsgifunc()
+
+class levels:
+    level_map = {
+        'wilderness': 'wilderness.json'
+        }
+
+    def GET(self, name):
+        web.header('Content-Type', 'application/json')
+        data = 'Level not found'
+        if self.level_map.get(name, False):
+            f = open(self.level_map[name])
+            data = f.readlines()
+            f.close()
+
+        return data
+
+if __name__ == '__main__':
+    app.run()
